377953bf9219c584e899d4021378cd12fad30228
Enis Mulić <enis.s.mulic@gmail.com>
2021-02-24 22:58:42 +0100

    Init project

---
+++ src/FitCommunity.User.Service.Core/IUserDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FitCommunity.User.Service.Core
+{
+    public interface IUserDbContext
+    {
+        DbSet<Domain.User> Users { get; set; }
+    }
+}
---
+++ src/FitCommunity.User.Service.Domain/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitCommunity.User.Service.Domain
+{
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+}
