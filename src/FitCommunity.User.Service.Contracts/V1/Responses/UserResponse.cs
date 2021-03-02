c51257d40e5f0f8cca84f0d53ccfea30da26948e
Enis Mulić <enis.s.mulic@gmail.com>
2021-03-02 22:10:35 +0100

    Add user service setup

---
+++ src/FitCommunity.User.Service.Contracts/V1/Responses/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FitCommunity.User.Service.Contracts.V1.Responses
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/FitCommunity.User.Service.Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using FitCommunity.User.Service.Contracts.V1.Requests;
+using FitCommunity.User.Service.Contracts.V1.Responses;
+using FitCommunity.User.Service.Core;
+using FitCommunity.User.Service.Core.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace FitCommunity.User.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserDbContext _dbContext;
+
+        public UserService(IUserDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> Get(UserSearchRequest search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> Insert(UserUpsertRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> Update(Guid id, UserUpsertRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
