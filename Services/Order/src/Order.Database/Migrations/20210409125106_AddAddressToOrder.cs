0864ea5f8a33962a4332d2ed57add7434b42e906
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-09 15:09:53 +0200

    Add address to order

---
+++ Services/Order/src/Order.Database/Migrations/20210409125106_AddAddressToOrder.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Order.Database.Migrations
+{
+    public partial class AddAddressToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Country",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_State",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Street",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_ZipCode",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Country",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Address_State",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Street",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Address_ZipCode",
+                table: "Orders");
+        }
+    }
+}
