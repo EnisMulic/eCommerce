97d5c30593687eca437d0d2ec24c0b840cb53c7e
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-09 19:48:22 +0200

    Add setup for commands and queries

---
+++ Services/Order/src/Order.Core/Mappings/OrderProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Order.Contracts.Responses;
+
+namespace Order.Core.Mappings
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Domain.Order, OrderResponse>();
+        }
+    }
+}
---
+++ Services/Order/src/Order.Core/Queries/GetOrderByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Order.Contracts.Responses;
+using System;
+
+namespace Order.Core.Queries
+{
+    public class GetOrderByIdQuery : IRequest<OrderResponse>
+    {
+        public Guid Id { get; }
+
+        public GetOrderByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
