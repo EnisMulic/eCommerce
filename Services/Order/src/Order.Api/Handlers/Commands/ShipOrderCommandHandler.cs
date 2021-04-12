e0373d66536c633a74020f12c3ba2707683765d9
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-12 21:50:08 +0200

    Implement ShipOrder command handler

--- Services/Order/src/Order.Api/Handlers/Commands/ShipOrderCommandHandler.cs
+++ Services/Order/src/Order.Api/Handlers/Commands/ShipOrderCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Order.Contracts.Responses;
 using Order.Core.Commands;
+using Order.Core.Helpers;
+using Order.Database;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +11,24 @@ namespace Order.Api.Handlers.Commands
 {
     public class ShipOrderCommandHandler : IRequestHandler<ShipOrderCommand, IResponse>
     {
-        public Task<IResponse> Handle(ShipOrderCommand request, CancellationToken cancellationToken)
+        private readonly OrderDbContext _context;
+        private readonly IResponseBuilder<Domain.Order> _responseBuilder;
+
+        public ShipOrderCommandHandler(OrderDbContext context, IResponseBuilder<Domain.Order> responseBuilder)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+            _responseBuilder = responseBuilder;
+        }
+
+        public async Task<IResponse> Handle(ShipOrderCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Set<Domain.Order>()
+                .SingleOrDefaultAsync(i => i.Id == request.Id);
+
+            entity.SetShippedStatus();
+            await _context.SaveChangesAsync();
+
+            return _responseBuilder.Create<OrderResponse>(entity);
         }
     }
 }
