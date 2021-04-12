a27d4d2edca46982457dc43ca58b169a62b29491
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-12 21:47:05 +0200

    Implement CancelOrder command handler

--- Services/Order/src/Order.Api/Handlers/Commands/CancelOrderCommandHandler.cs
+++ Services/Order/src/Order.Api/Handlers/Commands/CancelOrderCommandHandler.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Order.Contracts.Responses;
 using Order.Core.Commands;
+using Order.Core.Helpers;
+using Order.Database;
+using Order.Domain;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +12,24 @@ namespace Order.Api.Handlers.Commands
 {
     public class CancelOrderCommandHandler : IRequestHandler<CancelOrderCommand, IResponse>
     {
-        public Task<IResponse> Handle(CancelOrderCommand request, CancellationToken cancellationToken)
+        private readonly OrderDbContext _context;
+        private readonly IResponseBuilder<Domain.Order> _responseBuilder;
+
+        public CancelOrderCommandHandler(OrderDbContext context, IResponseBuilder<Domain.Order> responseBuilder)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+            _responseBuilder = responseBuilder;
+        }
+
+        public async Task<IResponse> Handle(CancelOrderCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Set<Domain.Order>()
+                .SingleOrDefaultAsync(i => i.Id == request.Id);
+
+            entity.SetCancelledStatus();
+            await _context.SaveChangesAsync();
+
+            return _responseBuilder.Create<OrderResponse>(entity);
         }
     }
 }
