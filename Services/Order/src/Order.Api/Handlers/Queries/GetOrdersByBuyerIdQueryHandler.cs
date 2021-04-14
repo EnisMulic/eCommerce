bbaf7bd08d98c592afa4834065c7265ba3b1f9c3
Enis Mulić <enis.s.mulic@gmail.com>
2021-04-14 19:39:13 +0200

    Implement get orders by BuyerId

--- Services/Order/src/Order.Api/Handlers/Queries/GetOrdersByBuyerIdQueryHandler.cs
+++ Services/Order/src/Order.Api/Handlers/Queries/GetOrdersByBuyerIdQueryHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Order.Contracts.Requests;
 using Order.Contracts.Responses;
 using Order.Core.Helpers;
 using Order.Core.Queries;
 using Order.Database;
-using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,9 +22,24 @@ namespace Order.Api.Handlers.Queries
             _responseBuilder = responseBuilder;
         }
 
-        public Task<IResponse> Handle(GetOrdersByBuyerIdQuery request, CancellationToken cancellationToken)
+        public async Task<IResponse> Handle(GetOrdersByBuyerIdQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var query = _context.Set<Domain.Order>()
+                .Include(i => i.OrderItems)
+                .Include(i => i.OrderStatus)
+                .Where(i => i.BuyerId == request.BuyerId)
+                .AsNoTracking()
+                .AsQueryable();
+
+            var total = await query.CountAsync(cancellationToken);
+
+            query = query.Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize);
+
+            var list = await query.ToListAsync(cancellationToken);
+
+            var pagination = new PaginationQuery(request.PageNumber, request.PageSize);
+            return _responseBuilder.Create<PagedResponse<OrderResponse>>(list, total, pagination);
         }
     }
 }
