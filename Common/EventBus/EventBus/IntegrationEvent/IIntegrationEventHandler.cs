a25ed9552aa85a22243d060a49d44467a0322161
Enis Mulić <enis.s.mulic@gmail.com>
2021-05-16 22:45:22 +0200

    Add event bus structure

---
+++ Common/EventBus/EventBus/IntegrationEvent/IIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EventBus
+{
+    public interface IIntegrationEventHandler<T> where T : IntegrationEvent
+    {
+        Task Handle(T @event);
+    }
+}
---
+++ Common/EventBus/EventBus/SubscriptionManager/IEventBusSubscriptionsManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventBus
+{
+    public interface IEventBusSubscriptionsManager
+    {
+        bool IsEmpty { get; }
+        event EventHandler<string> OnEventRemoved;
+        void AddSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void RemoveSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void AddDynamicSubscription<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+        void RemoveDynamicSubsciption<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+        bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+        bool HasSubscriptionForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
+        void Clear();
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+        string GetEventKey<T>();
+    }
+}
