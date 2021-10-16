c85a3347481df61001d16edda743c9e559f5e47a
Enis Mulić <enis.s.mulic@gmail.com>
2021-10-16 12:01:27 +0200

    Use customerId in basket repository

--- Services/Basket/src/Basket.Domain/BasketItem.cs
+++ Services/Basket/src/Basket.Domain/BasketItem.cs
@@ -4,7 +4,6 @@ namespace Basket.Domain
 {
     public class BasketItem
     {
-        public Guid Id { get; set; }
         public Guid ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal UnitPrice { get; set; }
